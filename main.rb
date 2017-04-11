b2695ad111d784e78640487e94b9f9b1cc02915b
Vinay Balaji <vinaybalaji@gmail.com>
2017-04-11 04:38:02 +0000

    Created player class and tested data attributes from main.rb. Also fixed spacing issue in VB Code

--- main.rb
+++ main.rb
@@ -1,7 +1,10 @@
 require_relative 'player'
 
-player1 = Player.new()
+player1 = Player.new
 
+puts player1.total_lives
 puts player1.current_lives
-puts player1.reduce_life
+player1.reduce_life
+puts player1.current_lives
+player1.reduce_life
 puts player1.current_lives
--- player.rb
+++ player.rb
@@ -1,14 +1,15 @@
 class Player
-attr_reader :total_lives
-attr_accessor :current_lives
+  attr_reader :total_lives
+  attr_accessor :current_lives
 
-    def intitialize()
-        @total_lives = 3
-        @current_lives = 3
-    end
+  def initialize
+    @total_lives = 3
+    @current_lives = 3
+  end
 
-    def reduce_life
-        if @current_lives > 0 ? @current_lives-- : @current_lives = 0
-    end
-    
+  def reduce_life
+    @current_lives -= 1
+    @current_lives = 0 if @current_lives < 0
+  end
+
 end
